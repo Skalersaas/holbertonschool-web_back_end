630a480d537e9db48285307f6cc9ac369931bec7
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 15:47:56 +0400

    Update 1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -5,6 +5,6 @@
     str2 : string two
 '''
 
-def concat(str1:str, str2:str) -> str:
+def concat(str1: str, str2: str) -> str:
     ''' Returns concatenated string from str1 and str2'''
     return str1 + str2
