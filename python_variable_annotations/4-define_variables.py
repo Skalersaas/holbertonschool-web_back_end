470051fc6bc228e97a12412badd07909216e84e2
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 17:08:59 +0400

    Update 4-define_variables.py

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-
+''' Description: We have 4 variables with different classes
+    a: int
+    pi: float
+    i_understand_annotations: bool
+    school: str
+'''
 
 a = 1
 pi = 3.14
