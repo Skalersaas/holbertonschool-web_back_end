5220ff5a464a3664bd8f705cd79c1d7c6d138bd1
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 17:31:37 +0400

    Update 8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,6 +2,7 @@
 ''' Description: Type-annotated function make_multiplier
     that takes a float multiplier as argument
     and returns a function that multiplies a float by multiplier.
+    Arguments: multiplier: float
 '''
 from typing import Callable
 
