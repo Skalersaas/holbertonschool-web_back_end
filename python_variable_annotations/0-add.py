2926569c38cc12bff704e1aee37a20316bad795b
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 15:25:18 +0400

    Update 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python3
-'''
-    Adds two numbers with floating point
-'''
 
 def add(a: float,b: float) -> float:
+    '''Returns sum of two numbers with floating point'''
     return a + b
