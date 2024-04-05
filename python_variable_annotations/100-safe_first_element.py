88e012d77ece283a88585b9ec61af044bbe8c709
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 19:17:10 +0400

    Update 100-safe_first_element.py

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python3
-''' Description: Type-annotated function safe_first_element
-    that takes a list lst as argument
-    and Checks if list exist, if yes returns first element.
-    Arguments: lst: Sequence[Any]
-'''
-
-from typing import Optional, Sequence, Any
+"""This module contains an annotated function that
+returns info about a sequence"""
+from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: list) -> Optional[Any]:
-    """
-    Returns the first element of the list if it exists, otherwise returns None.
-    
-    Args:
-        lst (list): The input list.
-    
-    Returns:
-        Optional[Any]: The first element of the list or None if the list is empty.
-    """    
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Return the first element of a sequence or none"""
     if lst:
         return lst[0]
     else:
         return None
-    
