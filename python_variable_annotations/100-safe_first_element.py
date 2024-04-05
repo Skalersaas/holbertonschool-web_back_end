4773e7f0d9de4654024960e641b1dfd3ca6b4a04
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 18:01:02 +0400

    Update 100-safe_first_element.py

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -8,9 +8,11 @@
 from typing import Optional, Sequence, Any
 
 
-def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+class Duck:
     '''Checks if list exist, if yes returns first element'''
-    if lst:
-        return lst[0]
-    else:
-        return None
+    def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+        '''Checks if list exist, if yes returns first element'''
+        if lst:
+            return lst[0]
+        else:
+            return None
