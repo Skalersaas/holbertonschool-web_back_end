30116f34b5017dbb60c02e7446badb4f03dca88a
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 17:30:57 +0400

    Create 8-make_multiplier.py

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' Description: Type-annotated function make_multiplier
+    that takes a float multiplier as argument
+    and returns a function that multiplies a float by multiplier.
+'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''Returns sum of all elements in list'''
+    def multiply_by(m: float)-> float:
+        return multiplier * m
+    return multiply_by
