efa40af8e14740aaa0f4497f954b30600dbd3274
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 20:44:02 +0400

    Update 0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,10 @@ yields random numbers once in a second
 '''
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     '''yields random numbers once in a second'''
     for _ in range(10):
         await asyncio.sleep(1)
