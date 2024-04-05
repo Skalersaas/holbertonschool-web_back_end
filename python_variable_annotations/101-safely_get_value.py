3c4cd57d21028e0b100acddbc46f427e91633604
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 19:26:00 +0400

    sa

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
-''' Description: Type-annotated function safe_first_element
-    that takes a list lst as argument
-    and Checks if list exist, if yes returns first element.
-    Arguments: lst: Sequence[Any]
-'''
-
+"""This module contains a type annotated function that uses TypeVar"""
 from typing import Mapping, Any, Union, TypeVar
-
-
-
 T = TypeVar("T")
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+
+
+def safely_get_value(
+    dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """Returns a value from a dictionary"""
     if key in dct:
         return dct[key]
     else:
