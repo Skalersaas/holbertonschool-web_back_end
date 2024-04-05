73a1d6190a2af760741d89d818261cfb970f6289
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 17:27:03 +0400

    Update 7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,7 +4,7 @@
     and an int OR float v as arguments
     and returns a tuple.
     The first element of the tuple is the string k.
-    The second element is the square of the int/float v 
+    The second element is the square of the int/float v
     and should be annotated as a float.
 '''
 from typing import Tuple, Union
