0df0d956db45c482c2e7f11cd717b3b434f82fc1
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 20:47:22 +0400

    Update 1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,12 +3,10 @@
 Description: contains function that
 yields random numbers once in a second
 '''
-import asyncio
-import random
-from typing import Generator
+from typing import List
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension () -> Generator[float, None, None]:
+async def async_comprehension () -> List[float]:
     '''yields random numbers once in a second'''
     return async_generator()
