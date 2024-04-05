c2b98e8de3e3dadda777bd84a3333b87c6d9cbd7
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 17:44:03 +0400

    Create 9-element_length.py

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' Description: Type-annotated function element_length
+    that takes a list lst as argument
+    and returns a List of tuples with all elements and their lengths.
+    Arguments: lst: Iterable[Sequence]
+'''
+
+from typing import Iterable, List, Tuple, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence,int]]:
+    '''returns a List of tuples with all elements and their lengths'''
+    return [(i, len(i)) for i in lst]
