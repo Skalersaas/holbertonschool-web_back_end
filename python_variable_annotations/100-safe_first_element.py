e482ab80ef6c78542eb0f0a720b762f2f8222d9c
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 18:10:42 +0400

    Update 100-safe_first_element.py

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -8,7 +8,7 @@
 from typing import Optional, Sequence, Any
 
 
-def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+def safe_first_element(lst: list) -> Optional[Any]:
     """
     Returns the first element of the list if it exists, otherwise returns None.
     
