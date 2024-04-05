0081a210c419affd3aed8f199c90d388302d48b4
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 20:58:15 +0400

    Update 2-measure_runtime.py

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""The coroutine will collect 10 random numbers
+using an async comprehensing over async_generator,
+then return the 10 random numbers.
+"""
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    start = time.time()
+    for _ in range(4):
+        await asyncio.gather(async_comprehension())
+    return time.time() - start
