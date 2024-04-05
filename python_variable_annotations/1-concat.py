3914d41aa6bf4fcc945aa842b2a4935ab6beaef5
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 15:53:58 +0400

    Update 1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,11 +2,10 @@
 ''' Description: Type-annotated function concat
     that takes a string str1 and a string str2
     as arguments and returns a concatenated string
-    Arguments:
-    str1 : string one
-    str2 : string two
+    Arguments: a : string 
+    b : string 
 '''
 
 def concat(str1: str, str2: str) -> str:
-    ''' Returns concatenated string from str1 and str2'''
+    '''Returns concatenated string from str1 and str2'''
     return str1 + str2
