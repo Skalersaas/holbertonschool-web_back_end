77ed4976003cea85cb6ea55c5ed00a31a0b83916
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 21:21:05 +0400

    Update 0-basic_cache.py

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -7,11 +7,15 @@ BaseCaching = __import__("base_caching").BaseCaching
 class BasicCache(BaseCaching):
     ''' Basic cache class
     '''
+
+
     def put(self, key, item):
         """ Add an item in the cache
         """
         if key and item:
             self.cache_data[key] = item
+
+
     def get(self, key):
         """ Get an item by key
         """
