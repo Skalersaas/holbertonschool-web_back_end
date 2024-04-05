499924aac0ec72464a6a9c56f68d9382c96930bf
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-05 20:53:22 +0400

    Update 1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-'''
-Description: contains function that
-yields random numbers once in a second
-'''
+"""The coroutine will collect 10 random numbers
+using an async comprehensing over async_generator,
+then return the 10 random numbers.
+"""
 
 from typing import List
 
-async_generator = __import__("0-async_generator").async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension () -> List[float]:
-    '''yields random numbers once in a second'''
+async def async_comprehension() -> List[float]:
+    '''async function returns list of random numbers'''
     return [i async for i in async_generator()]
