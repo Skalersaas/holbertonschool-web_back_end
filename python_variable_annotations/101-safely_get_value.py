d5a8c0d4ca9a3b1fb5f7f03f5dbcd11396d1eceb
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-06 23:24:28 +0400

    Update 101-safely_get_value.py

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
-"""This module contains a type annotated function that uses TypeVar"""
-from typing import Mapping, Any, Union, TypeVar
-T = TypeVar("T")
+"""
+This is a module that provides a function for safely getting a value from
+    a mapping.
+"""
+from typing import Any, Mapping, Union, TypeVar
+
+T = TypeVar('T')
+Res = Union[Any, T]
 Def = Union[T, None]
-Ret = Union[Any, T]
 
 
-def safely_get_value(dct: Mapping, key: Any,
-                     default: Def = None) -> Ret:
-    """Returns a value from a dictionary"""
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
+    """
+    This function gets the value for a given key in a mapping, or returns
+        a default value if the key is not in the mapping.
+    """
     if key in dct:
         return dct[key]
     else:
