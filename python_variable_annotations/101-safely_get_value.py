f904c6bcc5c9fdc1ab3359da475497651651748b
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-06 23:23:40 +0400

    Update 101-safely_get_value.py

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -2,10 +2,12 @@
 """This module contains a type annotated function that uses TypeVar"""
 from typing import Mapping, Any, Union, TypeVar
 T = TypeVar("T")
+Def = Union[T, None]
+Ret = Union[Any, T]
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, None] = None) -> Union[Any, T]:
+                     default: Def = None) -> Ret:
     """Returns a value from a dictionary"""
     if key in dct:
         return dct[key]
