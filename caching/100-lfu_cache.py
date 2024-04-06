272fcf03d31b1a4d17804437395516e82e694b2c
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-06 23:04:38 +0400

    Update 100-lfu_cache.py

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 ''' Basic cache Basic cache Basic cache Basic cache
 '''
 
@@ -7,7 +8,10 @@ from base_caching import BaseCaching
 class LFUCache(BaseCaching):
     ''' MRU cache class
     '''
-    list = {}
+    def __init__(self):
+        super().__init__()
+        self.listC = {}
+        
     def put(self, key, item):
         ''' Add an item in the cache
             and remove first if more than max items
@@ -15,10 +19,11 @@ class LFUCache(BaseCaching):
         if key and item:
             self.cache_data[key] = item
             if self.cache_data.__len__() > BaseCaching.MAX_ITEMS:
-                first = next(iter(self.cache_data))
+                first = sorted(self.listC.items(), key = lambda x:x[1])[0][0]
                 print("DISCARD:", first)
                 self.cache_data.pop(first)
-            list[key] = 0
+                self.listC.pop(first)
+            self.listC[key] = 0
 
     def get(self, key):
         ''' Get an item by key
@@ -27,41 +32,5 @@ class LFUCache(BaseCaching):
             value = self.cache_data[key]
             self.cache_data.pop(key)
             self.cache_data[key] = value
-            list[key] += 1
+            self.listC[key] += 1
             return value
-my_cache = LFUCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-print(my_cache.get("B"))
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
-my_cache.put("H", "H")
-my_cache.print_cache()
-my_cache.put("I", "I")
-my_cache.print_cache()
-print(my_cache.get("I"))
-print(my_cache.get("H"))
-print(my_cache.get("I"))
-print(my_cache.get("H"))
-print(my_cache.get("I"))
-print(my_cache.get("H"))
-my_cache.put("J", "J")
-my_cache.print_cache()
-my_cache.put("K", "K")
-my_cache.print_cache()
-my_cache.put("L", "L")
-my_cache.print_cache()
-my_cache.put("M", "M")
-my_cache.print_cache()
