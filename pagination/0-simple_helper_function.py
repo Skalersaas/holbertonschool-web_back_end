5a8f5ef33041292da1a85d5c664f0b3a43a1b973
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-10 20:42:19 +0400

    Create 0-simple_helper_function.py

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This file contains function that  return
+a tuple of size two containing a start index and an end index
+corresponding to the range of indexes to return
+in a list for those particular pagination parameters.
+"""
+
+def index_range(page, page_size):
+    '''method'''
+    return ((page-1)*page_size, page*page_size)
