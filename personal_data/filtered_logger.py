ef2bdb1a5a404b95a3aadf900e550dc4ce88cb95
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-16 13:29:59 +0400

    Update filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -21,13 +21,13 @@ class RedactingFormatter(logging.Formatter):
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
-    fields = []
+
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        logging.basicConfig(format = self.FORMAT)
+        '''Formatting'''
         return filter_datum(self.fields, self.REDACTION,
-                         super(RedactingFormatter, self).format(record),
-                         self.SEPARATOR)
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
