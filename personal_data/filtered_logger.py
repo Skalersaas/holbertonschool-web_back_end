d7db567af0c271e3db0a9363e473ce4704ee2713
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-16 13:39:54 +0400

    Update filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -31,3 +31,15 @@ class RedactingFormatter(logging.Formatter):
         return filter_datum(self.fields, self.REDACTION,
                             super(RedactingFormatter, self).format(record),
                             self.SEPARATOR)
+
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    
+    stream = logging.StreamHandler()
+    stream.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    logger.addHandler(stream)
+    return logger
+PII_FIELDS = ("name", "email", "ssn", "password", "date_of_birth")
