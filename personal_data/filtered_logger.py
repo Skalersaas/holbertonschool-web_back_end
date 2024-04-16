223302ade4303549e1eef0ea0006be19c89bbe5c
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-16 13:27:31 +0400

    Update filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
+'''#!/usr/bin/env python3'''
 '''Filtered logger'''
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -11,3 +12,22 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(f"{item}=.*?{separator}",
                          f"{item}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+    fields = []
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        logging.basicConfig(format = self.FORMAT)
+        return filter_datum(self.fields, self.REDACTION,
+                         super(RedactingFormatter, self).format(record),
+                         self.SEPARATOR)
