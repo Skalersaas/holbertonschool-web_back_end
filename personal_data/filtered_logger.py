473d3022febc74785bb28e44076e30ca90dbb2d6
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-16 13:08:25 +0400

    Update filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,8 +4,11 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> List[str]:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> List[str]:
     '''Filtering'''
     for item in fields:
-        message = re.sub(f"{item}=.*?{separator}",f"{item}={redaction}{separator}",message)
+        message = re.sub(f"{item}=.*?{separator}",
+                         f"{item}={redaction}{separator}",
+                         message)
     return message
