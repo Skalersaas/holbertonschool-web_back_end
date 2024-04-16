9f368dde076af3046184ead8fff2ffd0cd89b5e9
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-16 14:10:49 +0400

    Update encrypt_password.py

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+'''Module for encrypting'''
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
+    '''Returns hash'''
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
