d7f91ca6ccdc29a03906315156434ca49387735f
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-21 17:14:50 +0400

    Update basic_auth.py

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,7 @@
 """ Module for auth
 """
 from api.v1.auth.auth import Auth
-import base64
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -19,3 +19,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ Decoding
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            return b64decode(base64_authorization_header).decode()
+        except Exception:
+            return None
