5ac93e4823f58918c9dc1f4ada3a15d73db20093
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-21 22:42:05 +0400

    Update app.py

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -20,6 +20,9 @@ if AUTH_TYPE == "basic_auth":
 elif AUTH_TYPE == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
+elif AUTH_TYPE == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionAuth()
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
