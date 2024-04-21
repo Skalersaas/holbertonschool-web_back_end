6e9190bfd2f41e583ca8eda8094e8537a03954b1
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-21 16:39:45 +0400

    Update app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -23,7 +23,7 @@ def beforeRequest(request):
     '''Before each request'''
     if auth is None:
         return
-    if auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+    if not auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
         return
     if auth.authorization_header(request) is None:
         abort(401)
