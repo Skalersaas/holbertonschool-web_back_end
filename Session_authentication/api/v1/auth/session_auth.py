02824811966ba3fbe02969293adec4927e60a78e
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-21 19:04:29 +0400

    Update session_auth.py

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -17,3 +17,10 @@ class SessionAuth(Auth):
         id = str(uuid.uuid4())
         self.user_id_by_session_id[id] = user_id
         return id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Gets user
+        """
+        if session_id is None or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
