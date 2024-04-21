9fa953f5dd5a58937e31fa2c893dca4699388cb8
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-21 18:55:05 +0400

    Update session_auth.py

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """ Module for auth
 """
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """ Child"""
+
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None or type(user_id) is not str:
+            return None
+        id = uuid.uuid4()
+        self.user_id_by_session_id[id] = user_id
+        return id
+    
