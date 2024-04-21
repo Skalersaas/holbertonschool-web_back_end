0fbe58c1ce9083f1b70e308c84c6f60628aa3acf
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-21 17:03:57 +0400

    Update basic_auth.py

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,18 @@
 """ Module for auth
 """
 from api.v1.auth.auth import Auth
-
+import base64
 
 class BasicAuth(Auth):
     """ Child"""
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ Extracting
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startsWith("Basic "):
+            return None
+        return authorization_header[6:]
