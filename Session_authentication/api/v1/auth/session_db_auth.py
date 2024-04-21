744f5a1b9bb683c4207e3d3e73819ce22720fc46
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-21 22:57:30 +0400

    main

---
+++ Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+""" Module for auth
+"""
+from models.user_session import UserSession
+from api.v1.auth.session_exp_auth import SessionExpAuth
+
+
+class SessionAuth(SessionExpAuth):
+    """ Child"""
+    
+    def create_session(self, user_id=None):
+        """ Save and create
+        """
+        s_id = super().create_session(user_id)
+        if not s_id:
+            return None
+        kwargs = {'user_id': user_id, "session_id": s_id}
+        session = UserSession(**kwargs)
+        session.save()
+        return s_id
+    def user_id_for_session_id(self, session_id=None):
+        """User ID for Session ID Database"""
+        if session_id is None:
+            return None
+
+        UserSession.load_from_file()
+        user_session = UserSession.search({
+            'session_id': session_id
+        })
+
+        if not user_session:
+            return None
+
+        user_session = user_session[0]
+
+        expired_time = user_session.created_at + \
+            timedelta(seconds=self.session_duration)
+
+        if expired_time < datetime.utcnow():
+            return None
+
+        return user_session.user_id
+
+    def destroy_session(self, request=None):
+        """Remove Session from Database"""
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if not user_id:
+            return False
+
+        user_session = UserSession.search({
+            'session_id': session_id
+        })
+
+        if not user_session:
+            return False
+
+        user_session = user_session[0]
+
+        try:
+            user_session.remove()
+            UserSession.save_to_file()
+        except Exception:
+            return False
+
+        return True
