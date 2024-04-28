eb1d5eedb0703c169a37294ad69ad87b5b60e4c1
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-28 11:59:27 +0400

    second

---
+++ user_authentication_service/db.py
@@ -0,0 +1,34 @@
+"""DB module
+"""
+from sqlalchemy import create_engine, insert
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import User, Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        user = User(email=email,
+                    hashed_password=hashed_password,
+                    session_id=self._session())
+        return user
--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """ Base User Model """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base as Base
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 
-class User(Base()):
+class User(Base):
     """User class"""
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
