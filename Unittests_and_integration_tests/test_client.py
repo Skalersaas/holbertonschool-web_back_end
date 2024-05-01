d5dbc43c7c1d7071c57fbfbd6a034ac196262939
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-01 16:23:40 +0400

    Update test_client.py

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
-"""Testing client"""
-from unittest import TestCase, mock
-from parameterized import parameterized
+""" Test the utils """
+
+
+import unittest
+from unittest.mock import patch, Mock, PropertyMock, call
+from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
 
 
-class TestGithubOrgClient(TestCase):
-    """Testing github client"""
+class TestGithubOrgClient(unittest.TestCase):
+    """ Test that json can be got """
 
     @parameterized.expand([
         ("google", {"google": True}),
         ("abc", {"abc": True})
     ])
-    @mock.patch('client.get_json')
-    def test_org(self, org, expected, got):
-        """ Testing org"""
-        got.return_value = expected
+    @patch('client.get_json')
+    def test_org(self, org, expected, get_patch):
+        """ Test the org of the client """
+        get_patch.return_value = expected
         x = GithubOrgClient(org)
         self.assertEqual(x.org, expected)
-        got.assert_called_once("https://api.github.com/orgs/"+org)
+        get_patch.assert_called_once_with("https://api.github.com/orgs/"+org)
