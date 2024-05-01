2c7922da9054d8c46fa084bc6f44a70f943c2137
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-01 15:30:19 +0400

    first

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Test utils module"""
+from unittest import TestCase
+from parameterized import parameterized, parameterized_class
+
+
+class TestAccessNestedMap(TestCase):
+    """Base class"""
+    
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, input, map, output):
+        """Joriki ejji"""
+        return self.assertEqual(input, map, output)
