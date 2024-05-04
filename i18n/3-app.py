935f6513ff5f6c69f1f313bb69fd769f37f9b95c
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-04 17:58:44 +0400

    Update 3-app.py

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -16,12 +16,11 @@ class Config(object):
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
     """Get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 
 
 @app.route('/')
