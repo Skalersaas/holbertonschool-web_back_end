445af503dc3277c07ae24552706097f0465c4366
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-04 17:58:07 +0400

    third

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -14,14 +14,14 @@ class Config(object):
 
 
 app.config.from_object(Config)
+babel = Babel(app)
 
-
+@babel.localeselector
 def get_locale():
     """Get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
