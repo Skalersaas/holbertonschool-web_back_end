8ba3099463eb543e005ab293f5bb7931fa6bd44e
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-04 17:53:02 +0400

    Update 3-app.py

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -5,29 +5,30 @@ from flask_babel import Babel, _
 
 app = Flask(__name__)
 
+
 class Config(object):
     """Setups"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-app.config.from_object(Config)
 
+app.config.from_object(Config)
 
 
 def get_locale():
     """Get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 babel = Babel(app, locale_selector=get_locale)
+
 
 @app.route('/')
 def home():
     """ Home Page
     """
     return render_template('3-index.html')
-
-
 
 
 if __name__ == "__main__":
