677caa028ab894a145fb98eba179b5cff817048c
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-15 20:09:22 +0400

    Update exercise.py

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,7 +3,7 @@
 """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache():
@@ -13,9 +13,15 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Storing"""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]) -> Union[str, bytes, int, float]:
+        """Get"""
+        if fn:
+            return fn(key)
+
+        return self._redis.get(key)
