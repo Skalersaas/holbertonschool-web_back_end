08ffeea6ccd09d650739297c67f6de5843f6ac72
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-15 20:27:12 +0400

    Update exercise.py

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -11,6 +11,7 @@ def count_calls(method: Callable) -> Callable:
     key = method.__qualname__
     @wraps(method)
     def wrapper(self, *args, **kwargs):
+        '''wrapper'''
         self._redis.incr(key)
         return method(self, args, kwargs)
     return wrapper
