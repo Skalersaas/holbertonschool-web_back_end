72e023df17b05a1e616247c23c534d0098b50305
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-18 21:17:22 +0400

    Update 5-valid_email.sql

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -3,4 +3,4 @@
 CREATE TRIGGER CHECK_EMAIL
     AFTER INSERT ON users
     FOR EACH ROW
-    valid_email = email REGEXP "^[a-zA-Z0-9][a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]*?[a-zA-Z0-9._-]?@[a-zA-Z0-9][a-zA-Z0-9._-]*?[a-zA-Z0-9]?\\.[a-zA-Z]{2,63}$"
+    SET valid_email = email REGEXP "^[a-zA-Z0-9][a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]*?[a-zA-Z0-9._-]?@[a-zA-Z0-9][a-zA-Z0-9._-]*?[a-zA-Z0-9]?\\.[a-zA-Z]{2,63}$"
