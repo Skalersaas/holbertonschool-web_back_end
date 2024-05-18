f3966091bdc3087977ee4e391f5442e40132d5ec
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-18 21:18:52 +0400

    Update 5-valid_email.sql

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,6 +1,6 @@
 -- Write a SQL script that creates a trigger that resets the attribute valid_email only when the email has been changed.
 
 CREATE TRIGGER CHECK_EMAIL
-    AFTER INSERT ON users
+    AFTER UPDATE ON users
     FOR EACH ROW
     SET valid_email = email REGEXP "^[a-zA-Z0-9][a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]*?[a-zA-Z0-9._-]?@[a-zA-Z0-9][a-zA-Z0-9._-]*?[a-zA-Z0-9]?\\.[a-zA-Z]{2,63}$"
