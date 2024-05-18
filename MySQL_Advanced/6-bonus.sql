baf36770b0408bf6e1e9f6e29a3ca0cb27325cca
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-18 21:51:08 +0400

    Update 6-bonus.sql

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -12,3 +12,4 @@ BEGIN
     END IF
     SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
     INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score)
+END;
