e307e9f44831b9c154008f20acdc9a3687b9fe9e
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-26 16:19:40 +0400

    Delete ES6-basic directory

--- ES6-basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const final = [];
-  for (const idx of array) {
-    final.push(appendString + idx);
-  }
-
-  return final;
-}
