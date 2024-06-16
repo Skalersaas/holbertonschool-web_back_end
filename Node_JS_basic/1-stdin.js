fe440c42f2d5fc1dfedd41a9be5af5796a146290
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-06-16 17:12:13 +0400

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+console.log("Welcome to Holberton School, what is your name?");
 
 process.on("exit", () => {
-    console.log("This important software is now closing")
+    console.log("This important software is now closing");
 });
 
 process.stdin.on("data", data => {
