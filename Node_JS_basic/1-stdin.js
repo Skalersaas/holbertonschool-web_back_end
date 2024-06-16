5910ecc1c1009d65ac7a4f9dad1a0aa05eed0266
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-06-16 17:17:46 +0400

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,11 @@
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.on("exit", () => {
-    console.log("This important software is now closing");
-});
+process.stdin.setEncoding('utf8');
 
 process.stdin.on("data", data => {
-    data = data.toString();
-    console.log("Your name is:", data);
+    process.stdout.write(`Your name is: ${data}`);
+});
+
+process.on("exit", () => {
+    process.stdout.write("This important software is now closing");
 });
